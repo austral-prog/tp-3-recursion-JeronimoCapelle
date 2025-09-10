f8e5e38fbe4255163bf8b7e5def9d7dcf92b527c
InfiniteRideC <jero.capelle@gmail.com>
2025-09-10 14:39:14 -0300

    Finished

--- src/main/java/com/recursion/RecursiveFunctions.java
+++ src/main/java/com/recursion/RecursiveFunctions.java
@@ -5,42 +5,66 @@ import java.util.List;
 public class RecursiveFunctions {
 
     public static int recursiveIndexOf(List<String> list, String target) {
-        return -1;
+        if (!list.contains(target)) {return -1;}
+
+        if (!list.getFirst().equals(target)) {
+            return 1 + recursiveIndexOf(list.subList(1, list.size()), target);
+        }
+        return 0;
     }
 
     public static int recursiveIndexOfByIndex(List<String> list, String target, int index) {
-        return -1;
+        int res = recursiveIndexOf(list.subList(index, list.size()), target);
+        if  (res != -1) {return res + index;} return res;
     }
 
+
     public static int recursiveIndexOfEmpty(List<String> list) {
-        return -1;
+        return recursiveIndexOf(list, "");
     }
 
     public static int recursivePut(String target, List<String> list) {
-        return -1;
+        int index = recursiveIndexOf(list,"");
+        if (index != -1) {
+        list.set(index, target);
+        }
+        return index;
     }
 
     public static int recursiveRemove(List<String> list, String target) {
-        return -1;
+        if (!list.contains(target)) {return 0;}
+
+        if (list.getFirst().equals(target)) {
+            return 1 + recursiveRemove(list.subList(1, list.size()), target);
+        }
+        return recursiveRemove(list.subList(1, list.size()),target);
     }
 
     public static int recursiveSum(List<Integer> list) {
-        return -1;
+        if (list.isEmpty()) {return 0;}
+        return list.getFirst() + recursiveSum(list.subList(1, list.size()));
     }
 
     public static int recursiveFactorial(int n) {
-        return -1;
+        if (n == 0) return 1;
+        return n * recursiveFactorial(n - 1);
     }
 
     public static int recursivePow(int base, int exponent) {
-        return -1;
+        if (exponent == 0) return 1;
+        return base * recursivePow(base, exponent-1);
     }
 
     public static int recursiveFibonacci(int n) {
-        return -1;
+        if (n <= 1) return n;
+        return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
     }
 
     public static boolean recursivePalindrome(String word) {
+        if (word.length() == 1) return true;
+        if (word.charAt(0) == word.charAt(word.length() - 1)) {
+            return recursivePalindrome(word.substring(1, word.length() - 1));
+        }
         return false;
     }
 
